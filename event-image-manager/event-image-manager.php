6129efef4f54121e086baa61e413038b7ab8d0b5
galanakis vasileios marios <130051902+galanakisbm@users.noreply.github.com>
2026-02-23 14:21:31 +0200

    Initial commit of event-image-manager.php

---
+++ event-image-manager/event-image-manager.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: Event Image Manager
+ * Plugin URI: 
+ * Description: A plugin to manage event images.
+ * Version: 1.0
+ * Author: Galanakisbm
+ * Author URI: 
+ */
+
+// Your code goes here
